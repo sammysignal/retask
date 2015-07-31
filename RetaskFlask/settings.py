27fcfaad79c3777281db31160b8b359fa4a5319d
Sameer Mehra <sammysignal@gmail.com>
2015-07-31 10:38:20 -0400

    settings and gitignore

---
+++ RetaskFlask/settings.py
@@ -0,0 +1,12 @@
+
+
+# configuration
+DEBUG = True
+SECRET_KEY = 'Q\xfd\n-r\x13V#_\x84\xbc>\x90ck\xb3\x83\xcaw\x81 \xaby7'
+
+MAIL_SERVER='smtp.gmail.com'
+MAIL_PORT=465
+MAIL_USE_TLS = False
+MAIL_USE_SSL= True
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''

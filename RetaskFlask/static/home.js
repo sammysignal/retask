cff75b3a0995588b748293ecca0bd0d9f0e4dacb
Sameer Mehra <smehra@hubspot.com>
2015-07-31 15:22:31 -0400

    trying to get email data to display properly

--- RetaskFlask/static/home.js
+++ RetaskFlask/static/home.js
@@ -1,5 +1,15 @@
-$(document).ready(
-
-
-
-);
+$(document).ready(function () {
+  $('.email-bar').click(function () {
+      var emailData = this.getElementsByClassName('email-data')[0].innerHTML;
+      console.log(emailData);
+      var bodyChunks = emailData.split("'");
+      console.log("bC = " + JSON.stringify(bodyChunks));
+      var chunkString = "";
+      for (var i = 0; i < bodyChunks.length; i++) {
+        if (!bodyChunks[i] == ", " && !bodyChunks[i] == "[" && !bodyChunks[i] == "]") {
+          chunkString = chunkString + bodyChunks[i];
+        }
+      }
+      document.write(chunkString);
+  });
+});
